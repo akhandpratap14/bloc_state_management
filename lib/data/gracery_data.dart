6bf7113fd746bd31dbfe2920be0952ce136f373d
Akhand Pratap Rai <akhandpratap892@gmail.com>
2024-06-05 20:06:36 +0530

    bloc initail event and state on home done

--- lib/data/gracery_data.dart
+++ lib/data/gracery_data.dart
@@ -2,73 +2,73 @@ class GroceryData {
   static List<Map<String, dynamic>> groceryProducts = [
     {
       "id": "1",
-      "product_name": "Apple",
+      "name": "Apple",
       "category": "Fruits",
       "price": 0.99,
       "description": "Fresh Red Delicious Apples",
-      "image_url":
+      "imgUrl":
           "https://healthjade.com/wp-content/uploads/2017/10/apple-fruit.jpg"
     },
     {
       "id": "2",
-      "product_name": "Bread",
+      "name": "Bread",
       "category": "Bakery",
       "price": 2.50,
       "description": "Whole Grain Bread Loaf",
-      "image_url":
+      "imgUrl":
           "https://upload.wikimedia.org/wikipedia/commons/3/33/Fresh_made_bread_05.jpg"
     },
     {
       "id": "3",
-      "product_name": "Milk",
+      "name": "Milk",
       "category": "Dairy",
       "price": 1.79,
       "description": "Homogenized Whole Milk",
-      "image_url":
+      "imgUrl":
           "https://as1.ftcdn.net/v2/jpg/01/06/68/88/1000_F_106688812_rVoRFXazgIMEUJdvffG9p0XvP8Lntf0a.jpg"
     },
     {
       "id": "4",
-      "product_name": "Carrot",
+      "name": "Carrot",
       "category": "Vegetables",
       "price": 0.45,
       "description": "Organic Carrots",
-      "image_url":
+      "imgUrl":
           "https://www.sahajaseeds.com/wp-content/uploads/2022/11/VEGETABLE-CARROT-Carrot-Red-1.jpg"
     },
     {
       "id": "5",
-      "product_name": "Cheese",
+      "name": "Cheese",
       "category": "Dairy",
       "price": 3.99,
       "description": "Cheddar Cheese Block",
-      "image_url": "https://m.media-amazon.com/images/I/61+AzywctoL._SX679_.jpg"
+      "imgUrl": "https://m.media-amazon.com/images/I/61+AzywctoL._SX679_.jpg"
     },
     {
       "id": "6",
-      "product_name": "Eggs",
+      "name": "Eggs",
       "category": "Dairy",
       "price": 2.29,
       "description": "Large Brown Eggs",
-      "image_url":
+      "imgUrl":
           "https://nutritionsource.hsph.harvard.edu/wp-content/uploads/2012/09/FPG_06-EggsCarton_FeaturedImage.jpg"
     },
     {
       "id": "7",
-      "product_name": "Flour",
+      "name": "Flour",
       "category": "Grains",
       "price": 3.49,
       "description": "All-Purpose Flour",
-      "image_url":
+      "imgUrl":
           "https://www.thespruceeats.com/thmb/9nqbunVKWIDQ-nM33k3Q8Cpc56Y=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/all_purpose_flour1_2-92747f05d2c7445d86fedae894c8c16e.jpg"
     },
     {
       "id": "8",
-      "product_name": "Grapes",
+      "name": "Grapes",
       "category": "Fruits",
       "price": 2.99,
       "description": "Seedless Green Grapes",
-      "image_url":
+      "imgUrl":
           "https://smartyield.in/wp-content/uploads/2021/06/Green-grape.png"
     }
   ];
--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -12,6 +12,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    homeBloc.add(HomeInitialEvent());
+    super.initState();
+  }
+
   final HomeBloc homeBloc = HomeBloc();
   @override
   Widget build(BuildContext context) {
@@ -29,35 +35,46 @@ class _HomeState extends State<Home> {
         }
       },
       builder: (context, state) {
-        return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.teal,
-            title: const Text('Grocery App'),
-            actions: [
-              IconButton(
-                  onPressed: () {
-                    homeBloc.add(HomeWishlistButtonNavigateEvent());
-                  },
-                  icon: const Icon(Icons.favorite_border)),
-              IconButton(
-                  onPressed: () {
-                    homeBloc.add(HomeCartButtonNavigateEvent());
-                  },
-                  icon: const Icon(Icons.shopping_bag_outlined)),
-            ],
-          ),
-          body: Container(
-            height: 800,
-            width: 500,
-            color: Colors.black,
-            child: const Center(
-              child: Text(
-                'Home',
-                style: TextStyle(color: Colors.white),
+        switch (state.runtimeType) {
+          case HomeLoadingStating:
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          case HomeLoadedSuccessState:
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.teal,
+                title: const Text('Grocery App'),
+                actions: [
+                  IconButton(
+                      onPressed: () {
+                        homeBloc.add(HomeWishlistButtonNavigateEvent());
+                      },
+                      icon: const Icon(Icons.favorite_border)),
+                  IconButton(
+                      onPressed: () {
+                        homeBloc.add(HomeCartButtonNavigateEvent());
+                      },
+                      icon: const Icon(Icons.shopping_bag_outlined)),
+                ],
               ),
-            ),
-          ),
-        );
+              body: Container(
+                height: 800,
+                width: 500,
+                color: Colors.black,
+                child: const Center(
+                  child: Text(
+                    'Home',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            );
+          case HomeErrorState:
+            return const Scaffold(body: Center(child: Text('Error')));
+          default:
+            return const SizedBox();
+        }
       },
     );
   }
